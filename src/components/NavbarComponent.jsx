cd86988f9375f547c05e54f8343e4ba905d7e748
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-09 00:11:07 +0530

    Adding Routes and HomePage UI with Navbar Style

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,7 +1,14 @@
+import styles from "../style/navbar.module.css"
 function NavbarComponent() {
-    return <>
-     Navbar
-    </>
+    return <div className={styles.navbarWrapper}>
+     <div>Movie Base</div>
+     <div className={styles.searchBar}>
+        <input type="text" placeholder="What Movie Your are Thinking about...."  />
+     </div>
+     <div>
+        Theme
+     </div>
+    </div>
 }
 
 export default NavbarComponent; 
