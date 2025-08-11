5991518b3704aa23c5d6151279a47ae16668340a
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-11 18:49:01 +0530

    Implement the AutoComplete Component

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,14 +1,32 @@
-import styles from "../style/navbar.module.css"
+import { useRef } from "react";
+import styles from "../style/navbar.module.css";
 function NavbarComponent() {
-    return <div className={styles.navbarWrapper}>
-     <div>Movie Base</div>
-     <div className={styles.searchBar}>
-        <input type="text" placeholder="What Movie Your are Thinking about...."  />
-     </div>
-     <div>
-        Theme
-     </div>
+  const resultListRef = useRef(null);
+
+  return (
+    <div className={styles.navbarWrapper}>
+      <div>Movie Base</div>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          id={styles.movieSearchInput}
+          placeholder="What Movie Your are Thinking about...."
+          onFocus={() => {
+            resultListRef.current.style.display = "block";
+          }}
+          onBlur={() => {
+            resultListRef.current.style.display = "none";
+          }}
+        />
+        <div id={styles.resultList} ref={resultListRef}>
+          <div className={styles.autocompleteResult}>Result One</div>
+          <div className={styles.autocompleteResult}>Result Two</div>
+          <div>Result Three</div>
+        </div>
+      </div>
+      <div>Theme</div>
     </div>
+  );
 }
 
-export default NavbarComponent; 
+export default NavbarComponent;
