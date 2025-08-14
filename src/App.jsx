c23b27b6a0ef208d58dee8a99b12cfb3f161e099
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-15 01:15:14 +0530

    Application theme feature completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
-import NavbarComponent from './components/NavbarComponent'
-import Mainroutes from './routes/Mainroutes'
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import Mainroutes from "./routes/Mainroutes";
+import ThemeContext from "./context/ThemeContext";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
 
   return (
-    <> 
-    <NavbarComponent/>
-    <Mainroutes />
-    </>
-  )
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div id={"app-wrapper"} data-theme={theme}>
+        <NavbarComponent />
+        <Mainroutes />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/ThemeComponent.jsx
+++ src/components/ThemeComponent.jsx
@@ -1,12 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faMoon} from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import style from "../style/theme.module.css";
+import { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 function ThemeComponent() {
-    return <div className={style.themeWrapper}>
-     <FontAwesomeIcon icon={faMoon} />
-    </div>
-}
+  const { theme, setTheme } = useContext(ThemeContext);
 
+  function toggleTheme() {
+     setTheme(theme === "dark" ? "light" : "dark");
+  }
+  return (
+    <div className={style.themeWrapper} onClick={toggleTheme}>
+      <FontAwesomeIcon
+        icon={theme === "dark" ? faSun : faMoon}
+        className={style.themeIcon}
+        
+      />
+    </div>
+  );
+}
 
 export default ThemeComponent;
